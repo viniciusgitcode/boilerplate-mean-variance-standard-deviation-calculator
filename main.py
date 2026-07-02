f43ffa3bae07047c9b7f1fd2a5ee9e12f32d155d
viniciusgitcode <79325686+viniciusgitcode@users.noreply.github.com>
2026-07-02 16:30:04 -0300

    solucao 01 Ainda falta o último teste, que é o de quando a lista não tem 9 elementos. Ainda não encontrei o motivo do erro.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import mean_var_std
 from unittest import main
 
+# print(mean_var_std.calculate([2,6,2,8,4,0,1,]))
 print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
 
 # Run unit tests automatically
